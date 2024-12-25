367e9983deeebd3bd7413b0ad3f44b784af72467
KeitaHoshino <106499621+starfieldKt@users.noreply.github.com>
2024-12-25 18:14:56 +0900

    READMEと日本語版READMEを追加し、ツールの説明と使い方を記載

---
+++ GSITileStitcher/translation_ja_JP.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+    <!--これは iRIC で使用する辞書ファイルです。
+<translation> と </translation> の間に、翻訳後の文字列を追記してください。
+-->
+    <context>
+        <message>
+            <source>GSI Tile Stitcher</source>
+            <translation>地理院タイル(写真)取得結合ツール</translation>
+        </message>
+        <message>
+            <source>Condition</source>
+            <translation>条件</translation>
+        </message>
+        <message>
+            <source>EPSG code</source>
+            <translation>EPSG(座標系)</translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS I</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS II</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS III</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS IV</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS V</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS VI</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS VII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS VIII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS IX</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS X</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XI</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XIII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XIV</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XV</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XVI</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XVII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XVIII</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>JGD2011 / Japan Plane Rectangular CS XIX</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>zoom level</source>
+            <translation>zoomレベル</translation>
+        </message>
+        <message>
+            <source>Output directory for tile images</source>
+            <translation>タイル画像の出力先</translation>
+        </message>
+        <message>
+            <source>Output filename for the merged file</source>
+            <translation>結合した画像の出力名</translation>
+        </message>
+    </context>
+</TS>
--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# GSITileStitcher
+# What is GSI Tile Stitcher
 
-iRIC上で作成した格子の範囲の地理院タイル画像をダウンロードし、位置情報もつくります。
+This tool allows you to download and stitch together Geospatial Information Authority (GSI) tiles (photos) at any zoom level within the grid range, and also creates a position information file for offline use of background images in the calculation area.
+
+## Installation
+
+Place the entire `GSITileStitcher` folder into `IRICROOT\private\solvers`.
+
+## Usage
+
+1. Create an appropriate grid for the area where you want the background image. (Rectangular grid generation tools are recommended)
+2. Enter the coordinate system, zoom level, and save location in the calculation conditions.
+3. Run the solver.
+
+
+# 地理院タイル(写真)取得結合ツールとは
+
+計算領域の背景画像をオフラインでも使いたいときに、格子の範囲の任意のズームレベルの地理院タイル(写真)をダウンロード、結合し位置情報ファイルも作ってくれるツールです。
+
+## インストール方法
+`GSITileStitcher`フォルダーを`IRICROOT\private\solvers`にフォルダごと入れてください。
+
+## 使い方
+
+1. 背景画像が欲しい範囲に適当な格子を作ります。（矩形格子生成ツールなどがおすすめ）
+2. 座標系、ズームレベル、保存先などを計算条件に入力します。
+3. ソルバーを実行します。
